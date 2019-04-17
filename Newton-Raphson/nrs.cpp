f364249a73b8843d4ac9946a774a2e2e99814d8d
Shelby J. Haberman <haberman.statistics@gmail.com>
2019-04-17 09:19:30 +0300

    Add modified Newton-Raphson case
    Fix error in nr.cpp and add modified Newton-Raphson case for a single variable.

--- Newton-Raphson/nrs.cpp
+++ Newton-Raphson/nrs.cpp
@@ -1,62 +1,66 @@
-//Newton-Rapshon algorithm for function maximization.
-//Function and first two derivatives are f.value, f.der1, and f.der2<0,
-//Maximum number of iterations is maxit.
+//Modified Newton-Rapshon algorithm for function maximization.
+//Function and its first two derivatives are f.value, f.der1, and f.der2.  A substitute f.ders<0 for f.der2 is also used.  The maximum number of main iterations is maxit.
 //If change in approximation to maximum is less
 //than tol, then iterations cease.
 //The largest permitted step is stepmax>0.
 //The improvement check is b>1.
-//See Chapter 3 of Analysis of Frequency Data by S. J. Haberman for a very closely-related
-//algorithm.
+//The approach here is related to one in an article of S. J. Haberman in Sociological Methodology
+//in 1988.
 #include<cstdlib>
 #include<functional>
 #include<cmath>
 #include<float.h>
 using namespace std;
-#include "nr.h"
-nrvar nr(const int maxit,const double tol,const double start,
+#include "nrs.h"
+nrsvar nrs(const int maxit,const double tol,const double start,
          const double stepmax,const double b,
-         function<fd2(double)> f)
+         function<fd2s(double)> f)
 {
-    
-    
     double d,deltaf,lower,upper,x,y;
     int i;
-    nrvar varx,vary;
+    nrsvar varx,vary;
+// lower is lower bound for location of maximum if only one maximum exists.
+    lower=-DBL_MAX;
+// upper is upper bound for location of maximum if only one maximum exists.
+    upper=DBL_MAX;
 // x is old value.
     x=start;
-// lower is lower bound for location of maximum.
-    lower=-DBL_MAX;
-// upper is upper bound for location of maximum.
-    upper=DBL_MAX;
-    
-// varx is current location, function, first derivative, and second derivative.
-    varx=nrvarf(x,f);
+// varx is current location, function, first derivative, second derivative, and alternative
+// to second derivative.
+    varx=nrsvarf(x,f);
 // Stop if derivative is 0.
     if(varx.der1==0.0) return varx;
 // Up to maxit iterations.
     for(i=0;i<maxit;i++)
     {
 // The proposed step.
-        d=fmax(-stepmax,fmin(stepmax,-varx.der1/varx.der2));
-
+        if(varx.der2<0.0)
+        {
+            d=fmax(-stepmax,fmin(stepmax,-varx.der1/varx.der2));
+        }
+        else
+        {
+            d=fmax(-stepmax,fmin(stepmax,-varx.der1/varx.der2s));
+        }
 // The proposed new value.
         y=x+d;
 // New bounds.
         if(y>x)
         {
             lower=x;
-            if(y>upper)y=upper;
+            if(y>=upper)y=upper;
         }
-        else
+        if(y<x)
         {
             upper=x;
-            if(x<lower)y=lower;
+            if(y<=lower)y=lower;
         }
-// Get new function value, new derivative, and new second derivative.
-        vary=nrvarf(y,f);
+// Get new function value, new derivative, new second derivative,
+// and new alternative to second derivative.
+        
+        vary=nrsvarf(y,f);
 // Stop for 0 derivative.
         if(vary.der1==0.0) return vary;
-
 // Look for adequate progress.
         deltaf=vary.max-varx.max;
         if(deltaf>=b*fabs((y-x)*vary.der1))
@@ -78,13 +82,14 @@ nrvar nr(const int maxit,const double tol,const double start,
                     lower=y;
                 }
                 y=x+(y-x)*varx.der1/(varx.der1-vary.der1);
-                vary=nrvarf(y,f);
+                vary=nrsvarf(y,f);
             }
         }
-       
+        
         x=y;
         varx=vary;
     }
     return vary;
 }
+
 
