445d9aa960196e51214bec8b8b320d86437f7b01
Shelby J. Haberman <haberman.statistics@gmail.com>
2019-08-07 12:52:11 +0300

    Update for August 7, 2019

--- Newton-Raphson/nrbswv.cpp
+++ Newton-Raphson/nrbswv.cpp
@@ -1,60 +1,68 @@
-//Newton-Rapshon algorithm for function maximization.
+//Newton-Raphson algorithm for function maximization.
 //This function is concerned with maximization along a line for a real function
-//on a finite-dimensional vector space.
+//on a nonempty open convex subset of a finite-dimensional vector space.
 //The function input involves a proposed vector step v from a vector x.
 //The function is specified in the struct fd2 with elements value, grad (for gradient),
-//and hess (for Hessian matrix). The maximum of f.value(x+av) is maximized approximately over a
+//and hess (for Hessian matrix. The maximum of f.value(x+av) is maximized approximately over a
 //real.  The dot product of v and f.grad(x) is assumed positive.
-//The Hessian is always negative definite, and it is assumed that the function approaches
-//negative infinity whenever the norm of the argument approaches infinity.
+//Iit is assumed that the function approaches
+//negative infinity whenever the norm of the argument
+//approaches infinity or when the argument approaches the boundary of the convex set.
+//It is assumed that a unique critical point exists both
+//for the overall function and for the function along a
+//specific direction.
 //Maximum number of iterations is maxit.
 //The improvement check is b>1.
-//The final step is y.  The program uses the struct nrvvar with elements locmax, max, grad,
-//and hess.  For a vector x, locmax is x, max is f.value(x), grad is the value of the gradient at x,
-//and hess is the value of the Hessian at x.
+//The final step is y.  The program uses the struct nrvvarb with elements locmax, max, grad,
+//hess, and fin.  For vec x, locmax is x, max is f.value(x), grad is the value of the gradient at x,
+//hess is the value of the Hessian at x, and fin is a bool variable with value true if x is in
+//the convex set.  Similar definitions apply to vec y in the case of y.
 //See Chapter 3 of Analysis of Frequency Data by S. J. Haberman for a very closely-related
 //algorithm.
-//This algorithm works with nrv.cpp.
+//This algorithm works with nrbvs.cpp.
 #include<armadillo>
 #include<float.h>
 using namespace std;
 using namespace arma;
-struct fd2v
+struct fd2bv
 {
     double value;
     vec grad;
     mat hess;
+    bool fin;
 };
 
-struct nrvvar
+struct nrvvarb
 {
     vec locmax;
     double max;
     vec grad;
     mat hess;
+    bool fin;
 };
-nrvvar nrvvarf(vec x,function<fd2v(vec)> f);
+nrvvarb nrvvarbf(vec x,function<fd2bv(vec)> f);
 
 double davidon(double,double,double,double,double,double);
-double newton(double,double,double);
+double newtons(double,double,double);
 double modit(double,double,double,double,double);
 void rebound(double,double,double &,double &);
-nrvvar nrwv(const int maxit,
-         const double stepmax,const double b,vec v,nrvvar varx,
-         function<fd2v(vec)> f)
+nrvvarb nrbswv(const int maxit,
+         const double stepmax,const double b,vec v,nrvvarb varx,
+         function<fd2bv(vec)> f)
 {
     
     
-    double d,dirx,diry,dirz,dir2x,dir2y,dir2z,fx,fy,fz,lower,upper,xx,yy,zz;
+    double d,dirx,diry,dirz,dir2x,dir2y,dir2z;
+    double fx,fy,fz,lower,upper,xx,yy,zz;
     int i;
-    nrvvar vary,varz;
+    nrvvarb vary,varz;
 
     zz=0.0;
 // lower is lower bound for location of maximum.
     lower=0.0;
 // upper is upper bound for location of maximum.
     upper=DBL_MAX;
-    
+    if(!varx.fin) return varx;
 // Need value of f.value at x.
     fx=varx.max;
 // Current location.
@@ -68,12 +76,17 @@ nrvvar nrwv(const int maxit,
     for(i=0;i<maxit;i++)
     {
 // The proposed step.  Keep within range.
-        yy=newton(zz,dirz,dir2z);
+        yy=newtons(zz,dirz,dir2z);
 
         yy=modit(zz,yy,stepmax,lower,upper);
 // Get new function value, new derivative, and new second derivative.
         
-        vary=nrvvarf(varx.locmax+yy*v,f);
+        vary=nrvvarbf(varx.locmax+yy*v,f);
+        while(!vary.fin)
+        {
+            yy=0.5*yy;
+            vary=nrvvarbf(varx.locmax+yy*v,f);
+        }
         fy=vary.max;
         diry=dot(v,vary.grad);
         dir2y=dot(v,vary.hess*v);
@@ -94,7 +107,7 @@ nrvvar nrwv(const int maxit,
         {
 // Cubic interpolation.
             yy=davidon(zz,yy,fz,fy,dirz,diry);
-            vary=nrvvarf(varx.locmax+yy*v,f);
+            vary=nrvvarbf(varx.locmax+yy*v,f);
             diry=dot(v,vary.grad);
             if(diry==0.0)return vary;
             rebound(yy,diry,lower,upper);
--- Newton-Raphson/nrbwv.cpp
+++ Newton-Raphson/nrbwv.cpp
@@ -1,17 +1,20 @@
 //Newton-Rapshon algorithm for function maximization.
 //This function is concerned with maximization along a line for a real function
-//on a finite-dimensional vector space.
+//on a nonempty open convex subset of a finite-dimensional vector space.
 //The function input involves a proposed vector step v from a vector x.
-//The function is specified in the struct fd2 with elements value, grad (for gradient),
-//and hess (for Hessian matrix). The maximum of f.value(x+av) is maximized approximately over a
-//real.  The dot product of v and f.grad(x) is assumed positive.
+//The function is specified in the struct fd2b with elements value, grad (for gradient),
+//hess (for Hessian matrix), and fin (true if argument in set and false otherwise).
+//The function f.value(x+av) is maximized approximately over real numbers a such that
+//x+av is in the convex set.
+//The dot product of v and f.grad(x) is assumed positive.
 //The Hessian is always negative definite, and it is assumed that the function approaches
 //negative infinity whenever the norm of the argument approaches infinity.
 //Maximum number of iterations is maxit.
 //The improvement check is b>1.
-//The final step is y.  The program uses the struct nrvvar with elements locmax, max, grad,
-//and hess.  For a vector x, locmax is x, max is f.value(x), grad is the value of the gradient at x,
-//and hess is the value of the Hessian at x.
+//The final step is y.  The program uses the struct nrvvarb with elements locmax, max, grad,
+//hess, and fin.  For a vector x, locmax is x, max is f.value(x),
+//grad is the value of the gradient at x,
+//hess is the value of the Hessian at x, fin is true if x is in the set and false otherwise.
 //See Chapter 3 of Analysis of Frequency Data by S. J. Haberman for a very closely-related
 //algorithm.
 //This algorithm works with nrv.cpp.
@@ -19,36 +22,38 @@
 #include<float.h>
 using namespace std;
 using namespace arma;
-struct fd2v
+struct fd2bv
 {
     double value;
     vec grad;
     mat hess;
+    bool fin;
 };
 
-struct nrvvar
+struct nrvvarb
 {
     vec locmax;
     double max;
     vec grad;
     mat hess;
+    bool fin;
 };
-nrvvar nrvvarf(vec x,function<fd2v(vec)> f);
+nrvvarb nrvvarbf(vec x,function<fd2bv(vec)> f);
 
 double davidon(double,double,double,double,double,double);
 double newton(double,double,double);
 double modit(double,double,double,double,double);
 void rebound(double,double,double &,double &);
-nrvvar nrwv(const int maxit,
-         const double stepmax,const double b,vec v,nrvvar varx,
-         function<fd2v(vec)> f)
+nrvvarb nrbwv(const int maxit,
+         const double stepmax,const double b,vec v,nrvvarb varx,
+         function<fd2bv(vec)> f)
 {
     
     
     double d,dirx,diry,dirz,dir2x,dir2y,dir2z,fx,fy,fz,lower,upper,xx,yy,zz;
     int i;
-    nrvvar vary,varz;
-
+    nrvvarb vary,varz;
+    if(!varx.fin)return varx;
     zz=0.0;
 // lower is lower bound for location of maximum.
     lower=0.0;
@@ -73,7 +78,12 @@ nrvvar nrwv(const int maxit,
         yy=modit(zz,yy,stepmax,lower,upper);
 // Get new function value, new derivative, and new second derivative.
         
-        vary=nrvvarf(varx.locmax+yy*v,f);
+        vary=nrvvarbf(varx.locmax+yy*v,f);
+        while(!vary.fin)
+        {
+            yy=0.5*yy;
+            vary=nrvvarbf(varx.locmax+yy*v,f);
+        }
         fy=vary.max;
         diry=dot(v,vary.grad);
         dir2y=dot(v,vary.hess*v);
@@ -94,7 +104,7 @@ nrvvar nrwv(const int maxit,
         {
 // Cubic interpolation.
             yy=davidon(zz,yy,fz,fy,dirz,diry);
-            vary=nrvvarf(varx.locmax+yy*v,f);
+            vary=nrvvarbf(varx.locmax+yy*v,f);
             diry=dot(v,vary.grad);
             if(diry==0.0)return vary;
             rebound(yy,diry,lower,upper);
