d694a688d01bf604fad93570c48bd914d3b1ad44
Shelby J. Haberman <haberman.statistics@gmail.com>
2019-09-12 17:47:38 +0300

    Update for September 12, 2019

---
+++ quadrature/adapt.cpp
@@ -0,0 +1,23 @@
+//Transformation of integration points by transformation loc+scale*point
+//and dividing integration weights by scale.
+#include<armadillo>
+using namespace arma;
+using namespace std;
+struct pw
+{
+    vec points;
+    vec weights;
+};
+
+pw adapt(double loc,double scale,pw pws)
+
+{
+    
+    pw results=pws;
+    results.points=loc+scale*pws.points;
+    results.weights=pws.weights/scale;
+    
+    
+    return results;
+}
+
