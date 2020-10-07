ba897c621f30c7f36482a9c557b1397daf52d3c4
Shelby J. Haberman <haberman.statistics@gmail.com>
2020-10-07 20:26:48 +0300

    Update for November 7, 2020

--- continuous/normalv.cpp
+++ continuous/normalv.cpp
@@ -5,48 +5,45 @@
 //are unpacked with unpack.cpp to yield a d by d lower triangular matrix m with positive
 //diagonal elements. The density f(y) is det(m)g(v+my), where g is the density of the standard
 //multivariate normal distribution of dimension d.
-//
-
-
 #include<armadillo>
 using namespace arma;
-using namespace std;
-struct fd2bv
+struct f2v
 {
     double value;
     vec grad;
     mat hess;
-    bool fin;
 };
-
 struct vecmat
 {
     vec v;
     mat m;
 };
-vec pack(vecmat u);
-vecmat unpack(int,vec);
-fd2bv normalv(vec y,vec beta)
+vec pack(vecmat & u);
+vecmat unpack(int &,vec &);
+f2v normalv(vec & y,vec & beta)
 {
     double d;
-    int i,i1,j,j1,k,k1;
-    vec a,aa,aaa,z;
+    int i,i1,j,j1,k,k1,n;
+    vec a,z;
     vecmat b,dec;
-    fd2bv results;
-    dec=unpack(y.n_elem,beta);
-    d=double(y.n_elem);
+    f2v results;
+    results.grad.set_size(beta.n_elem);
+    results.hess.set_size(beta.n_elem,beta.n_elem);
+    n=y.n_elem;
+    dec=unpack(n,beta);
     z=dec.v+dec.m*y;
     a=diagvec(dec.m);
     if(min(a)<=0.0)
     {
-        results.fin=false;
+        results.value=datum::nan;
+        results.grad.fill(datum::nan);
+        results.hess.fill(datum::nan);
         return results;
     }
-    results.fin=true;
-    results.value=-0.5*d*log(2.0*datum::pi)-0.5*dot(z,z)+sum(log(a));
+    results.value=-0.5*dot(z,z)+sum(log(a));
     b.v=-z;
-    b.m=zeros(y.n_elem,y.n_elem);
-    for(i=0;i<y.n_elem;i++)
+    b.m.set_size(n,n);
+    for(i=0;i<n;i++)
     {
         b.m(i,i)=1.0/a(i)-z(i)*y(i);
         if(i>0)
@@ -58,9 +55,8 @@ fd2bv normalv(vec y,vec beta)
         }
     }
     results.grad=pack(b);
-    results.hess=zeros(beta.n_elem,beta.n_elem);
-    k=y.n_elem;
-    
+    results.hess.set_size(beta.n_elem,beta.n_elem);
+    k=n;
     for(i=0;i<y.n_elem;i++)
     {
         results.hess(i,i)=-1.0;
@@ -71,19 +67,14 @@ fd2bv normalv(vec y,vec beta)
             k=k+1;
         }
     }
-    
-    
-    for(i=0;i<y.n_elem;i++)
+    for(i=0;i<n;i++)
     {
-        
-        
         for(j=0;j<=i;j++)
         {
-            k=y.n_elem+j+(i+1)*i/2;
+            k=n+j+(i+1)*i/2;
             for(j1=0;j1<=j;j1++)
             {
-                k1=y.n_elem+j1+i*(i+1)/2;
-                
+                k1=n+j1+i*(i+1)/2;
                 results.hess(k,k1)=-y(j)*y(j1);
                 if(j1<j)
                 {
@@ -98,6 +89,5 @@ fd2bv normalv(vec y,vec beta)
             k=k+1;
         }
     }
-    
     return results;
 }
--- continuous/normalv1.cpp
+++ continuous/normalv1.cpp
@@ -5,48 +5,42 @@
 //are unpacked with unpack.cpp to yield a d by d lower triangular matrix m with positive
 //diagonal elements. The density f(y) is det(m)g(v+my), where g is the density of the standard
 //multivariate normal distribution of dimension d.
-//
-
-
 #include<armadillo>
 using namespace arma;
-using namespace std;
-struct fd1bv
+struct f1v
 {
     double value;
     vec grad;
-    
-    bool fin;
 };
-
 struct vecmat
 {
     vec v;
     mat m;
 };
-vec pack(vecmat u);
-vecmat unpack(int,vec);
-fd1bv normalv1(vec y,vec beta)
+vec pack(vecmat & u);
+vecmat unpack(int &,vec &);
+f1v normalv1(vec & y,vec & beta)
 {
     double d;
-    int i,j;
+    int i,j,n;
     vec a,z;
     vecmat b,dec;
-    fd1bv results;
-    dec=unpack(y.n_elem,beta);
-    d=double(y.n_elem);
+    f1v results;
+    results.grad.set_size(beta.n_elem);
+    n=y.n_elem;
+    dec=unpack(n,beta);
     z=dec.v+dec.m*y;
     a=diagvec(dec.m);
     if(min(a)<=0.0)
     {
-        results.fin=false;
+        results.value=datum::nan;
+        results.grad.fill(datum::nan);
         return results;
     }
-    results.fin=true;
-    results.value=-0.5*d*log(2.0*datum::pi)-0.5*dot(z,z)+sum(log(a));
+    results.value=-0.5*dot(z,z)+sum(log(a));
     b.v=-z;
-    b.m=zeros(y.n_elem,y.n_elem);
-    for(i=0;i<y.n_elem;i++)
+    b.m.set_size(n,n);
+    for(i=0;i<n;i++)
     {
         b.m(i,i)=1.0/a(i)-z(i)*y(i);
         if(i>0)
@@ -58,13 +52,5 @@ fd1bv normalv1(vec y,vec beta)
         }
     }
     results.grad=pack(b);
-    
-    
-    
-    
-    
-    
-     
-    
     return results;
 }
