4daa95b857d261e7af50b5b9c5c6842e7d50181a
Shelby J. Haberman <haberman.statistics@gmail.com>
2021-03-23 18:09:26 +0200

    Update of March 23, 2021

--- tools/vecsel.cpp
+++ tools/vecsel.cpp
@@ -5,43 +5,11 @@ using namespace arma;
 struct xsel
 {
     bool all;
-    ivec list;
+    uvec list;
 };
 vec vecsel(const xsel & xselect, const vec & y)
 {
     int d,i,j;
-    vec result;
-    if(xselect.all)
-    {
-        d=y.size();
-    }
-    else
-    {
-        d=xselect.list.size();
-    }
-    result.set_size(d);
-    if(xselect.all)
-    {
-        result=y;
-    }
-    else
-    {
-        if(d>0)
-        {
-            for(i=0;i<d;i++)
-            {
-                j=xselect.list(i);
-                if(j>=0&&j<d)
-                {
-                    result(i)=y(j);
-                }
-                else
-                {
-                    result.fill(datum::nan);
-                    return result;
-                }
-            }
-        }
-    }
-    return result;
+    if(xselect.all) return y;
+    return y.elem(xselect.list);
 }
