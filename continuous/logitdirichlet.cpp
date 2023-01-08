772fc6a86c22469349a4e6dd3fecd73ec9261f03
Shelby J. Haberman <haberman.statistics@gmail.com>
2023-01-08 17:41:40 -0500

    Update of January 8, 2023

---
+++ continuous/logitdirichlet.cpp
@@ -0,0 +1,68 @@
+//Log likelihood component and its gradient and hessian matrix
+//for logit-dirichlet model with response y and parameter vector beta
+//of dimennsion q = r+1, where r is the dimension of the observed vector
+//y.dresp.  All elements of beta are positive.  For a vector u is a vector of dimension
+//q with a Dirichlet distribution, y(i)=log(u(i)/u(q)).  If order is 0,
+//only the function is
+//found, if order is 1, then the function and gradient are found.
+//If order is 2,
+//then the function, gradient, and Hessian are returned.
+#include<armadillo>
+#include<boost/math/special_functions/digamma.hpp>
+#include<boost/math/special_functions/trigamma.hpp>
+using namespace arma;
+using namespace boost::math;
+struct f2v
+{
+    double value;
+    vec grad;
+    mat hess;
+};
+struct resp
+{
+  ivec iresp;
+  vec dresp;
+};
+f2v logitdirichlet(const int & order, const resp & y, const vec & beta)
+{
+    double dz,tz,zz,zzz;
+    int i,j,q;
+    f2v results;
+    q=beta.n_elem;
+    vec z(q);
+    if(order>0) results.grad.set_size(q);
+    if(order>1) results.hess.set_size(q,q);
+    if(min(beta)<=0.0)
+    {
+      results.value=datum::nan;
+      if(order>0) results.grad.fill(datum::nan);
+      if(order>1) results.hess.fill(datum::nan);
+      return results;
+    }
+    zz=sum(beta);
+    zzz=log(1.0+sum(exp(y.dresp)));
+    results.value=dot(beta.subvec(0,q-2),y.dresp)-zz*zzz+lgamma(zz);
+    for(i=0;i<q;i++) results.value=results.value-lgamma(beta(i));
+    if(order>0)
+    {
+        dz=digamma(zz);
+        for(i=0;i<q;i++)
+        {
+              results.grad(i)=-zzz+dz-digamma(beta(i));
+              if(i<q-1)results.grad(i)=results.grad(i)+y.dresp(i);
+        }
+    }
+    if(order>1)
+    {
+        tz=trigamma(zz);
+        for(i=0;i<q;i++)
+        {
+              for(j=0;j<q;j++)
+              {
+                   results.hess(i,j)=tz;
+                   if(i==j)results.hess(i,j)=results.hess(i,j)-trigamma(beta(i));
+              }
+        }
+    }
+    return results;
+}
--- genresp/genresp.cpp
+++ genresp/genresp.cpp
@@ -1,9 +1,12 @@
 //Log likelihood component, gradient, and hessian matrix
 //for response model with response y and parameter
 //vector beta. The struct variable choice governs the model selection
+//choice.type='B' yields a logit-beta model.
 //choice.type='C' yields a cumulative model, choice.type='D'
-//yields a continuous model, choice.type='G' yields
-//a graded model, choice.type='L' yields a multinomial logit model,
+//yields a continuous model, choice.type='E' yields a logit-dirichlet model,
+//choice.type='G' yields
+//a graded model, choice.type='H' yields a log-gamma model,
+//choice.type='L' yields a multinomial logit model,
 //choice.type='M' yields a model for the maximum of two independent Bernoulli
 //random variables.  choice.type='N' yields a multivariate normal model,
 //choice.type='P' yields a Poisson model with a logarithmic transformation,
@@ -42,6 +45,9 @@ f2v contresp(const int & , const char & , const resp & , const vec & );
 f2v cumresp(const int & , const char &, const resp &, const vec &) ;
 f2v gradresp(const int & , const char &, const resp &, const vec & );
 f2v maxberresp(const int & , const char &, const resp &, const vec & );
+f2v loggamma(const int & , const resp &, const vec & );
+f2v logitbeta(const int & , const resp &, const vec & );
+f2v logitdirichlet(const int & , const resp &, const vec & );
 f2v logmean(const int & , const resp &, const vec & );
 f2v maxberresp(const int & , const char & , const resp &, const vec & );
 f2v multlogit(const int & , const resp &, const vec & );
@@ -51,10 +57,13 @@ f2v truncresp(const int & , const char &, const resp & , const vec & );
 f2v genresp(const int & order, const model & choice, const resp & y, const vec & beta)
 {
     switch(choice.type)
-    {  
+    {
+        case 'B': return logitbeta(order, y, beta); 
         case 'C': return cumresp(order, choice.transform, y, beta);
         case 'D': return contresp(order, choice.transform, y, beta);
+        case 'E': return logitdirichlet(order, y, beta);
         case 'G': return gradresp(order, choice.transform, y, beta);
+        case 'H': return loggamma(order, y, beta);
         case 'L': return multlogit(order, y, beta);
         case 'M': return maxberresp(order, choice.transform, y, beta);
         case 'N': return normalv(order, y, beta);
