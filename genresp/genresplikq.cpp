1f50022daaf229b72f31c352f8d13973d6c9df36
Shelby J. Haberman <haberman.statistics@gmail.com>
2023-08-04 18:48:24 -0400

    Update of August 4, 2023

---
+++ genresp/genresplikq.cpp
@@ -0,0 +1,134 @@
+//Log likelihood and its gradient and Hessian
+//for response model with response y and
+//predictor matrix x.  Penalized likelihood is found
+//for quadratic penalty.
+//Weight w is used.
+//Model codes are found in genresp.cpp.
+//If order is 0, only the function is
+//found, if order is 1, then the function and gradient are found.  If order is 2,
+//then the function, gradient, and Hessian are returned.   If order is 3,
+//then the Hessian is replaced by the approximate Hessian
+//of the Louis method.
+//xselect indicates which predictors apply to which responses.
+//obssel indicates which observations to consider.
+//The penalty matrix is the symmetric and nonnegative-definite matrix Q.
+//The target vector is b.
+#include<armadillo>
+using namespace std;
+using namespace arma;
+struct f2v
+{
+    double value;
+    vec grad;
+    mat hess;
+};
+struct model
+{
+  char type;
+  char transform;
+};
+struct resp
+{
+  ivec iresp;
+  vec dresp;
+};
+struct xsel
+{
+  bool all;
+  uvec list;
+};
+struct dat
+{
+     model choice;
+     double weight;
+     resp dep;
+     vec offset;
+     mat indep;
+     xsel xselect;
+};
+f2v genresp(const int & , const model &, const resp &, const vec &);
+void addsel(const int & , const xsel & , const f2v & , f2v & , const double & );
+f2v linsel(const int & , const f2v & , const mat & );
+vec vecsel(const xsel & , const vec & );
+f2v genresplikq(const int & order, const vector<dat> & data, const xsel & obssel,
+    const mat & Q, const vec & b, const vec & beta)
+{
+    vec beta1, lambda, u, v;
+    f2v obsresults,linresults;
+    f2v results;
+    int i,ii,j,jj,k,kk,order0=0,order1, p,q,r,n,nn;
+    p=beta.n_elem;
+    n=data.size();
+    beta1.set_size(p);
+    beta1=b-beta;
+    results.value=-dot(beta1,Q*beta1);   
+    if(order>0)
+    {
+        results.grad.set_size(p);
+        results.grad=2.0*Q*beta1;
+    }
+    if(order>1)
+    {
+        results.hess.set_size(p,p);
+        results.hess=-2.0*Q;
+    }
+    order1=order;
+    if(order>2)order1=1;
+    if(obssel.all)
+    {
+         nn=n;
+    }
+    else
+    {
+        nn=obssel.list.size();
+    }
+    if(nn==0) return results;
+    for (ii=0;ii<nn;ii++)
+    {
+        if(obssel.all)
+        {
+             i=ii;
+        }
+        else
+        {
+             i=obssel.list(ii);
+        }
+        r=data[i].offset.n_elem;
+        q=data[i].indep.n_cols;
+        lambda.set_size(r);
+        lambda=data[i].offset;
+        if(order>0&&q>0)obsresults.grad.set_size(r);
+        if(order>1&&q>0)obsresults.hess.set_size(r,r);
+        if(q>0)
+        {
+            beta1.set_size(q);
+            beta1=vecsel(data[i].xselect,beta);
+            lambda=lambda+data[i].indep*beta1;
+            obsresults=genresp(order1, data[i].choice, data[i].dep, lambda);
+        }
+        else
+        {
+            obsresults=genresp(order0, data[i].choice, data[i].dep, lambda);
+        }
+        if(isnan(obsresults.value))
+        {
+            results.value=datum::nan;
+            if(order>0)results.grad.fill(datum::nan);
+            if(order>1)results.hess.fill(datum::nan);
+            return results;
+        }    
+        if(q==0||order==0)
+        {
+            addsel(order0,data[i].xselect,obsresults,results,data[i].weight);
+        }
+        else 
+        {
+            if(order>2)obsresults.hess=-obsresults.grad*obsresults.grad.t();
+            linresults.grad.set_size(q);
+            if(order>1) linresults.hess.set_size(q,q);
+            linresults=linsel(order,obsresults,data[i].indep);
+            addsel(order,data[i].xselect,linresults,results,data[i].weight);  
+        }
+    }
+    return results;
+}
---
+++ genresp/genrespmleq.cpp
@@ -0,0 +1,84 @@
+//Find maximum likelihood estimates for response model with quadratic penalty.
+//See genresplik.cpp and nrv.cpp for definitions of structs
+//used.
+//Newton-Raphson approach is used if algorithm is N.
+//Louis approach is used if algorithm is L.
+//Conjugate gradient method is used if algorithm is C.
+//Gradient ascent is used if algorithm is G.
+#include<armadillo>
+using namespace std;
+using namespace arma;
+struct f2v
+{
+    double value;
+    vec grad;
+    mat hess;
+};
+struct maxf2v
+{
+    vec locmax;
+    double max;
+    vec grad;
+    mat hess;
+};
+struct params
+{
+    bool print;
+    int maxit;
+    int maxits;
+    double eta;
+    double gamma1;
+    double gamma2;
+    double kappa;
+    double tol;
+};
+struct model
+{
+  char type;
+  char transform;
+};
+struct resp
+{
+  ivec iresp;
+  vec dresp;
+};
+struct xsel
+{
+  bool all;
+  uvec list;
+};
+struct dat
+{
+     model choice;
+     double weight;
+     resp dep;
+     vec offset;
+     mat indep;
+     xsel xselect;
+};
+maxf2v conjgrad(const int &, const params & , const vec & ,
+           const function<f2v(const int & , const vec & )> f);
+maxf2v gradascent(const int &, const params & , const vec & ,
+           const function<f2v(const int & , const vec & )> f);
+maxf2v nrv(const int &, const params & , const vec & ,
+           const function<f2v(const int & , const vec & )> f);
+
+f2v genresplikq(const int & , const vector<dat> & , const xsel & ,
+     const mat & , const vec & , const vec &);
+maxf2v genrespmleq(const int & order, const params & mparams,
+    const char & algorithm, const vector<dat> & data, const xsel & obssel,
+    const mat & Q,  const vec & b, const vec & start)
+{
+    maxf2v results;
+    int p;
+    p=start.n_elem;
+    results.locmax.set_size(p);
+    results.grad.set_size(p);
+    if(algorithm=='N'||algorithm=='L')results.hess.set_size(p,p);
+    auto f=[&data,&obssel,&Q,&b](const int & order,const vec & start)
+        {return genresplikq(order,data,obssel,Q,b,start);};
+    if(algorithm=='N'||algorithm=='L')results=nrv(order, mparams, start, f);
+    if(algorithm=='C')results=conjgrad(order, mparams, start, f);
+    if(algorithm=='G')results=gradascent(order, mparams, start, f);
+    return results;
+}
