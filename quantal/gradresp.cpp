96323b093f3abdca17ca1e60e542cfa73b41a181
Shelby J. Haberman <haberman.statistics@gmail.com>
2026-06-01 17:32:57 -0400

    Update of June 1, 2026

--- quantal/gradresp.cpp
+++ quantal/gradresp.cpp
@@ -1,8 +1,21 @@
 //Log likelihood component, gradient, and Hessian
 //for modified graded response model with response y with integer values
-//0 to n and parameter vector beta of dimension n.
-//Choice of tranformation is determined by transform, with 'G' for
-//complementary log-log, 'H' for log-log,
+//Y=y.iresp(0) from 0 to  to n and parameter vector beta of dimension n.
+//The distribution of Y involves an
+//unobserved continuous variable Z with positive twice continuously differentiable density,
+//distribution function F, quantile function T specified by transform, and n cut points t(k),
+//k a nonnegative integer less than n.
+//The cut points are strictly increasing in k.
+//The observed variable y.iresp(0) is the number of cut points
+//that are not less than Z. Let q be the largest integer no greater
+//than n/2.  The initial element beta(0) of beta is t(q), and
+//for k greater than 0 and less than n,
+//log(t(k))-t(k-1))) is beta(k).  Thus for k>q, t(k) is
+//beta(0) plus the sum for j from q+1 to k of exp(beta(j)).
+//For k<q, t(k) is beta(0) minus the sum for j from k+1 to q
+//of exp(beta(j)).
+//Choice of T has 'G' for the
+//complementary log-log transformation, 'H' for log-log,
 //'L' for logit, and 'N' for probit.
 //If order is 0, only the function is
 //found, if order is 1, then the function and gradient are found.
@@ -25,81 +38,169 @@ f2v berresp(const int & , const char & , const resp & , const vec & );
 f2v gradresp(const int & order, const char & transform ,
     const resp & y, const vec & beta)
 {
-    double e;
-    f2v results, resultp, resultq;
-    int i, n, nn, nnn;
-    resp z, zz;
-    z.iresp={0};
-    zz.iresp={1};
+    int n;
     n=beta.n_elem;
-    nn=n-y.iresp(0);
+//Bernoulli case.
+    if(n==1)return berresp(order, transform, y, beta);
+//result is for t(k) for k=n-y.iresp(0), and result1 is for
+//t(k-1).
+//results is for final result.
+    f2v result, result1, results;
     if(order>0)results.grad.zeros(n);
     if(order>1)results.hess.zeros(n,n);
-    if(nn==0){
-        resultp=berresp(order, transform, zz, beta);
-        results.value=resultp.value;
+    int k;
+    k=n-y.iresp(0);
+    resp z;
+    z.iresp={1};
+//lb and ub give bounds used for t(k) if k<n is not q, while lb1 and ub1 are for
+//bounds used for t(k-1) if k>0 is not q+1.
+//Bounds are 0 if not used.  sp is a range variable.
+    int lb=0, lb1=0, q, sp, ub=0, ub1=0;
+    q=n/2;
+//gamma is the parameter vector for t(k).
+//gamma1 is the parameter vector for t(k-1).
+//ebeta is used to find t(k).
+//ebeta1 is for t(k-1).
+    vec ebeta, ebeta1, gamma(1), gamma1(1);
+//Now for bounds.
+    if(k==n){
+        if(n==2)result1=berresp(order, transform, y, beta);
+//Note that n-q>1 if n>2.
+        else{
+            lb1=q+1;
+            ub1=n-1;
+            ebeta1=exp(beta.subvec(lb1,ub1));
+            gamma1(0)=beta(0)+sum(ebeta1);
+            result1=berresp(order, transform, y, gamma1);
+        }
+        results.value=result1.value;
         if(order==0)return results;
-        results.grad(0)=resultp.grad(0);
+        results.grad(0)=result1.grad(0);
+        if(n>2)results.grad.subvec(lb1,ub1)=result1.grad(0)*ebeta1;
         if(order==1)return results;
-        results.hess(0,0)=resultp.hess(0,0);
-        return results;
-    }
-    if(n==1){
-        resultp=berresp(order, transform, z, beta);
-        results.value=resultp.value;
-        if(order==0)return results;
-        results.grad(0)=resultp.grad(0);
-        if(order==1)return results;
-        results.hess(0,0)=resultp.hess(0,0);
-        return results;
-    }
-    nnn=std::min(nn+1,n);
-    vec gamma(1), ebeta(nnn), tau(nnn), etau(nnn),febeta(nnn);
-    ebeta=exp(beta.head(nnn));
-    tau(0)=beta(0);
-    etau(0)=ebeta(0);
-    for(i=1;i<nnn;i++){
-        etau(i)=etau(i-1)+ebeta(i);
-        tau(i)=log(etau(i));
-    }
-    if(order>0)febeta=(1.0/etau(nnn-1))*ebeta;
-    if(nn==n){
-        gamma(0)=tau(n-1);
-        resultp=berresp(order, transform, z, gamma);
-        results.value=resultp.value;
-        if(order==0)return results;
-        results.grad=resultp.grad(0)*febeta;
-        if(order==1)return results;
-        results.hess=(resultp.hess(0,0)-resultp.grad(0))*febeta*febeta.t()
-            +resultp.grad(0)*diagmat(febeta);
+        results.hess(0,0)=result1.hess(0,0);
+        if(n>2){
+            (results.hess.col(0)).subvec(lb1,ub1)=result1.hess(0,0)*ebeta1;
+            (results.hess.row(0)).subvec(lb1,ub1)=((results.hess.col(0)).subvec(lb1,ub1)).t();
+            results.hess.submat(lb1,lb1,ub1,ub1)=result1.hess(0,0)*ebeta1*ebeta1.t()+result1.grad(0)*diagmat(ebeta1);
+        }
         return results;
     }
     else{
-        vec d(2);
-        gamma(0)=tau(nn-1);
-        resultq=berresp(order,transform,zz,gamma);
-        d(1)=exp(resultq.value);
-        gamma(0)=tau(nn);
-        resultp=berresp(order,transform,zz,gamma);
-        d(0)=exp(resultp.value);
-        e=d(0)-d(1);
-        results.value=log(e);
-        if(order==0)return results;
-        vec f(2),gr(2),febeta1(nn);
-        f=(1.0/e)*d;
-        gr(0)=f(0)*resultp.grad(0);
-        gr(1)=-f(1)*resultq.grad(0);
-        febeta1=(1.0/etau(nn-1))*ebeta.head(nn);
-        results.grad.head(nnn)=gr(0)*febeta;
-        results.grad.head(nn)=results.grad.head(nn)+gr(1)*febeta1;
-        if(order==1)return results;
-        results.hess.submat(0,0,nn,nn)=f(0)*(resultp.hess(0,0)+resultp.grad(0)*resultp.grad(0))*febeta*febeta.t()
-           +gr(0)*(diagmat(febeta)-febeta*febeta.t());
-        results.hess.submat(0,0,nn-1,nn-1)=results.hess.submat(0,0,nn-1,nn-1)
-            -f(1)*(resultq.hess(0,0)+resultq.grad(0)*resultq.grad(0))*febeta1*febeta1.t()
-            +gr(1)*(diagmat(febeta1)-febeta1*febeta1.t());;
-        results.hess.submat(0,0,nn,nn)=results.hess.submat(0,0,nn,nn)
-            -results.grad.head(nnn)*results.grad.head(nnn).t();
+        if(k==0){
+            lb=1;
+            ub=q;
+            ebeta=-exp(beta.subvec(lb,ub));
+            gamma(0)=beta(0)+sum(ebeta);
+            result=berresp(order, transform, z, gamma);
+            results.value=result.value;
+            if(order==0)return results;
+            results.grad(0)=result.grad(0);
+            results.grad.subvec(lb,ub)=result.grad(0)*ebeta;
+            if(order==1)return results;
+            results.hess(0,0)=result.hess(0,0);
+            (results.hess.col(0)).subvec(lb,ub)=result.hess(0,0)*ebeta;
+            (results.hess.row(0)).subvec(lb,ub)=((results.hess.col(0)).subvec(lb,ub)).t();
+            results.hess.submat(lb,lb,ub,ub)=result.hess(0,0)*ebeta*ebeta.t()+result.grad(0)*diagmat(ebeta);
+            return results;
+        }
+        else{
+            if(k==q){
+                result=berresp(order, transform, z, beta);
+                lb1=q;
+                ub1=lb1;
+                ebeta1=-exp(beta.subvec(lb1,ub1));
+                gamma1(0)=beta(0)+ebeta1(0);
+                result1=berresp(order, transform, z, gamma1);
+            }
+            else{
+                if(k==q+1){
+                    lb=q+1;
+                    ub=lb;
+                    ebeta=exp(beta.subvec(lb,ub));
+                    gamma(0)=beta(0)+ebeta(0);
+                    result=berresp(order, transform, z, gamma);
+                    result1=berresp(order, transform, z, beta);
+                }
+                else{
+                    if(k>q+1){
+                        lb=q+1;
+                        ub=k;
+                        ebeta=exp(beta.subvec(lb,ub));
+                        gamma(0)=beta(0)+sum(ebeta);
+                        result=berresp(order, transform, z, gamma);
+                        lb1=lb;
+                        ub1=ub-1;
+                        sp=ub-lb;
+                        ebeta1=ebeta.head(sp);
+                        gamma1(0)=gamma(0)-ebeta(sp);
+                        result1=berresp(order, transform, z, gamma1);
+                    }
+                    else{
+                        ub=q;
+                        lb=k+1;
+                        ub1=ub;
+                        lb1=k;
+                        ebeta1=-exp(beta.subvec(lb1,ub1));
+                        gamma1(0)=beta(0)+sum(ebeta1);
+                        result1=berresp(order, transform, z, gamma1);
+                        sp=ub1-lb1;
+                        ebeta=ebeta1.tail(sp);
+                        gamma(0)=gamma1(0)-ebeta1(0);
+                        result=berresp(order, transform, z, gamma);
+                    }
+                }
+            }
+        }
+    }
+//Remaining cases with results not yet returned.
+    double d, d1, e, f, f1, g=0.0, g1=0.0, gr=0.0, gr1=0.0, gg, h=0.0, h1=0.0;
+    d=exp(result.value);
+    d1=exp(result1.value);
+    e=d-d1;
+    results.value=log(e);
+    if(order==0)return results;
+    f=d/e;
+    f1=d1/e;
+    gr=f*result.grad(0);
+    gr1=-f1*result1.grad(0);
+    results.grad(0)=gr+gr1;
+    if(lb>0)results.grad.subvec(lb,ub)=gr*ebeta;
+    if(lb1>0)results.grad.subvec(lb1,ub1)=results.grad.subvec(lb1,ub1)+gr1*ebeta1;
+    if(order==1) return results;
+    gg=results.grad(0);
+    g=gr*result.grad(0);
+    h=f*result.hess(0,0);
+    g1=gr1*result1.grad(0);
+    h1=-f1*result1.hess(0,0);
+    results.hess(0,0)=g+h+g1+h1-gg*gg;
+//Just upper bound.
+    if(lb>0&&lb1==0){
+        (results.hess.col(0)).subvec(lb,ub)=(g+h-gg*gr)*ebeta;
+        (results.hess.row(0)).subvec(lb,ub)=((results.hess.col(0)).subvec(lb,ub)).t();
+        results.hess.submat(lb,lb,ub,ub)=gr*diagmat(ebeta)+(g+h-gr*gr)*ebeta*ebeta.t();
         return results;
+    }
+//Just lower bound.
+    else{
+        if(lb1>0&&lb==0){
+            (results.hess.col(0)).subvec(lb1,ub1)=(g1+h1-gg*gr1)*ebeta1;
+            (results.hess.row(0)).subvec(lb1,ub1)=((results.hess.col(0)).subvec(lb1,ub1)).t();
+            results.hess.submat(lb1,lb1,ub1,ub1)=gr1*diagmat(ebeta1)+(g1+h1-gr1*gr1)*ebeta1*ebeta1.t();
+            return results;
+        }
+//Upper and lower bounds.
+        else{
+            (results.hess.col(0)).subvec(lb,ub)=(g+h-gg*gr)*ebeta;
+            (results.hess.row(0)).subvec(lb,ub)=((results.hess.col(0)).subvec(lb,ub)).t();
+            (results.hess.col(0)).subvec(lb1,ub1)=(results.hess.col(0)).subvec(lb1,ub1)+(g1+h1-gg*gr1)*ebeta1;
+            (results.hess.row(0)).subvec(lb1,ub1)=((results.hess.col(0)).subvec(lb1,ub1)).t();
+            results.hess.submat(lb,lb,ub,ub)=gr*diagmat(ebeta)+(g+h-gr*gr)*ebeta*ebeta.t();
+            results.hess.submat(lb1,lb1,ub1,ub1)=results.hess.submat(lb1,lb1,ub1,ub1)+gr1*diagmat(ebeta1)
+                +(g1+h1-gr1*gr1)*ebeta1*ebeta1.t();
+            results.hess.submat(lb,lb1,ub,ub1)=results.hess.submat(lb,lb1,ub,ub1)-gr*gr1*ebeta*ebeta1.t();
+            results.hess.submat(lb1,lb,ub1,ub)=results.hess.submat(lb1,lb,ub1,ub)-gr1*gr*ebeta1*ebeta.t();
+            return results;
+        }
     }
 }
